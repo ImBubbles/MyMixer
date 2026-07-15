010043c82745331583f9e3cd32439e7ceae8191f
Bubbles <bubblybubblesbubblesup@gmail.com>
2026-07-15 13:24:05 -0500

    Update Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -8,5 +8,7 @@ Application::Application(int argc, char *argv[]) : pipewireContext(PipeWireConte
     pipewireContext.createChannel("master", "Master");
     pipewireContext.createChannel("discord", "Discord");
     pipewireContext.linkChannels("discord", "master");
-    std::this_thread::sleep_for(std::chrono::seconds(60));
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    pipewireContext.unlinkChannels("discord", "master");
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 }
